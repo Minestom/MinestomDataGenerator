d1db268401a31870e7f59a322e32ecf37eaed4c0
TheMode <themode@outlook.fr>
2021-07-24 08:31:11 +0200

    Cleanup AttributeGenerator

--- DataGenerator/src/main/java/net/minestom/generators/AttributeGenerator.java
+++ DataGenerator/src/main/java/net/minestom/generators/AttributeGenerator.java
@@ -2,51 +2,28 @@ package net.minestom.generators;
 
 import com.google.gson.JsonObject;
 import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.RangedAttribute;
 import net.minestom.generators.common.DataGeneratorCommon;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.lang.reflect.Field;
-import java.util.Set;
 
 public final class AttributeGenerator extends DataGeneratorCommon {
-    private static final Logger LOGGER = LoggerFactory.getLogger(AttributeGenerator.class);
-
     @Override
     public JsonObject generate() {
-        Set<ResourceLocation> attributeRLs = Registry.ATTRIBUTE.keySet();
         JsonObject attributes = new JsonObject();
+        for (var entry : Registry.ATTRIBUTE.entrySet()) {
+            final var location = entry.getKey().location();
+            final var attribute = entry.getValue();
 
-        for (ResourceLocation attributeRL : attributeRLs) {
-            Attribute a = Registry.ATTRIBUTE.get(attributeRL);
-
-            JsonObject attribute = new JsonObject();
-            if (a == null) {
-                continue;
-            }
-            attribute.addProperty("translationKey", a.getDescriptionId());
-            attribute.addProperty("defaultValue", a.getDefaultValue());
-            attribute.addProperty("clientSync", a.isClientSyncable());
-            if (a instanceof RangedAttribute ra) {
-                // Unfortuantely get via reflection
+            JsonObject attributeJson = new JsonObject();
+            attributeJson.addProperty("translationKey", attribute.getDescriptionId());
+            attributeJson.addProperty("defaultValue", attribute.getDefaultValue());
+            attributeJson.addProperty("clientSync", attribute.isClientSyncable());
+            if (attribute instanceof RangedAttribute rangedAttribute) {
                 JsonObject range = new JsonObject();
-                try {
-                    Field maxV = RangedAttribute.class.getDeclaredField("maxValue");
-                    maxV.setAccessible(true);
-                    range.addProperty("maxValue", maxV.getDouble(ra));
-                    Field minV = RangedAttribute.class.getDeclaredField("minValue");
-                    minV.setAccessible(true);
-                    range.addProperty("minValue", minV.getDouble(ra));
-                } catch (IllegalAccessException | NoSuchFieldException e) {
-                    LOGGER.error("Failed to get attribute ranges, skipping attrobite with ID '" + attributeRL + "'.", e);
-                }
-                attribute.add("range", range);
+                range.addProperty("maxValue", rangedAttribute.getMaxValue());
+                range.addProperty("minValue", rangedAttribute.getMinValue());
+                attributeJson.add("range", range);
             }
-
-            attributes.add(attributeRL.toString(), attribute);
+            attributes.add(location.toString(), attributeJson);
         }
         return attributes;
     }
