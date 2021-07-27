51d8e7ea693014c8cf1036f5c40f42c35f8982e9
TheMode <themode@outlook.fr>
2021-07-27 08:11:43 +0200

    Save on disk space by assuming default values

--- DataGenerator/src/main/java/net/minestom/generators/MaterialGenerator.java
+++ DataGenerator/src/main/java/net/minestom/generators/MaterialGenerator.java
@@ -10,6 +10,7 @@ import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.SpawnEggItem;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minestom.generators.common.DataGeneratorCommon;
 
 public final class MaterialGenerator extends DataGeneratorCommon {
@@ -23,48 +24,56 @@ public final class MaterialGenerator extends DataGeneratorCommon {
             JsonObject itemJson = new JsonObject();
             itemJson.addProperty("id", Registry.ITEM.getId(item));
             itemJson.addProperty("translationKey", item.getDescriptionId());
-            itemJson.addProperty("depletes", item.canBeDepleted());
-            itemJson.addProperty("maxStackSize", item.getMaxStackSize());
-            itemJson.addProperty("maxDamage", item.getMaxDamage());
-            itemJson.addProperty("edible", item.isEdible());
-            itemJson.addProperty("fireResistant", item.isFireResistant());
-            itemJson.addProperty("blockId", Registry.BLOCK.getKey(Block.byItem(item)).toString());
-            ResourceLocation eatingSound = Registry.SOUND_EVENT.getKey(item.getEatingSound());
-            if (eatingSound != null) {
+            if (item.getMaxStackSize() != 64) { // Default = 64
+                itemJson.addProperty("maxStackSize", item.getMaxStackSize());
+            }
+            if (item.getMaxDamage() != 0) { // Default = 0 (no durability)
+                itemJson.addProperty("maxDamage", item.getMaxDamage());
+            }
+            if (item.isFireResistant()) { // Default = false
+                itemJson.addProperty("fireResistant", true);
+            }
+            // Corresponding block
+            Block block = Block.byItem(item);
+            if (block != Blocks.AIR) { // Default = no block
+                itemJson.addProperty("blockId", Registry.BLOCK.getKey(block).toString());
+            }
+            // Food properties
+            if (item.isEdible()) { // Default = false (not edible)
+                itemJson.addProperty("edible", true);
+                ResourceLocation eatingSound = Registry.SOUND_EVENT.getKey(item.getEatingSound());
                 itemJson.addProperty("eatingSound", eatingSound.toString());
-            }
-            ResourceLocation drinkingSound = Registry.SOUND_EVENT.getKey(item.getDrinkingSound());
-            if (drinkingSound != null) {
+                ResourceLocation drinkingSound = Registry.SOUND_EVENT.getKey(item.getDrinkingSound());
                 itemJson.addProperty("drinkingSound", drinkingSound.toString());
-            }
-            // Food Properties
-            if (item.getFoodProperties() != null) {
-                FoodProperties fp = item.getFoodProperties();
 
-                JsonObject foodProperties = new JsonObject();
-                foodProperties.addProperty("alwaysEdible", fp.canAlwaysEat());
-                foodProperties.addProperty("isFastFood", fp.isFastFood());
-                foodProperties.addProperty("nutrition", fp.getNutrition());
-                foodProperties.addProperty("saturationModifier", fp.getSaturationModifier());
-
-                {
-                    // Food effects
-                    JsonArray effects = new JsonArray();
-                    for (Pair<MobEffectInstance, Float> effect : fp.getEffects()) {
-                        ResourceLocation rl = Registry.MOB_EFFECT.getKey(effect.getFirst().getEffect());
-                        if (rl == null) {
-                            continue;
+                FoodProperties foodProperties = item.getFoodProperties();
+                if (foodProperties != null) {
+                    JsonObject foodPropertiesJson = new JsonObject();
+                    foodPropertiesJson.addProperty("alwaysEdible", foodProperties.canAlwaysEat());
+                    foodPropertiesJson.addProperty("isFastFood", foodProperties.isFastFood());
+                    foodPropertiesJson.addProperty("nutrition", foodProperties.getNutrition());
+                    foodPropertiesJson.addProperty("saturationModifier", foodProperties.getSaturationModifier());
+                    {
+                        // Food effects
+                        JsonArray effects = new JsonArray();
+                        for (Pair<MobEffectInstance, Float> effectEntry : foodProperties.getEffects()) {
+                            final var effect = effectEntry.getFirst();
+                            final var chance = effectEntry.getSecond();
+                            ResourceLocation rl = Registry.MOB_EFFECT.getKey(effect.getEffect());
+                            if (rl == null) {
+                                continue;
+                            }
+                            JsonObject foodEffect = new JsonObject();
+                            foodEffect.addProperty("id", rl.toString());
+                            foodEffect.addProperty("amplifier", effect.getAmplifier());
+                            foodEffect.addProperty("duration", effect.getDuration());
+                            foodEffect.addProperty("chance", chance);
+                            effects.add(foodEffect);
                         }
-                        JsonObject foodEffect = new JsonObject();
-                        foodEffect.addProperty("id", rl.toString());
-                        foodEffect.addProperty("amplifier", effect.getFirst().getAmplifier());
-                        foodEffect.addProperty("duration", effect.getFirst().getDuration());
-                        foodEffect.addProperty("chance", effect.getSecond());
-                        effects.add(foodEffect);
+                        foodPropertiesJson.add("effects", effects);
                     }
-                    foodProperties.add("effects", effects);
+                    itemJson.add("foodProperties", foodPropertiesJson);
                 }
-                itemJson.add("foodProperties", foodProperties);
             }
             // Armor properties
             if (item instanceof ArmorItem armorItem) {
