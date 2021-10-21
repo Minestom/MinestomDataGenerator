8392645f49fe3363b72a709b06d06d8705262c34
Articdive <13535885+Articdive@users.noreply.github.com>
2021-10-21 10:46:47 +0200

    Start using VanillaGradle and fix logging dependencies.

--- DataGenerator/build.gradle.kts
+++ DataGenerator/build.gradle.kts
@@ -3,6 +3,7 @@ group = "net.minestom"
 plugins {
     java
     application
+    id("org.spongepowered.gradle.vanilla") version "0.2"
 }
 
 repositories {
@@ -10,18 +11,16 @@ repositories {
 }
 
 dependencies {
-    // Logging
-    implementation("org.apache.logging.log4j:log4j-core:2.14.0")
-    // SLF4J is the base logger for most libraries, therefore we can hook it into log4j2.
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.0")
-    implementation("com.google.code.gson:gson:2.8.6")
-
-    implementation(files("../Deobfuscator/deobfuscated_jars/deobfu_1.17.1.jar"))
-
+    // SLF4J is the base logger for most libraries.
+    implementation("org.slf4j:slf4j-api:1.8.0-beta4")
+    implementation("com.google.code.gson:gson:2.8.8")
 }
-
-val mcVersion = project.properties["mcVersion"].toString()
 
 application {
     mainClass.set("net.minestom.datagen.DataGen")
+}
+
+minecraft {
+    version(project.rootProject.properties["mcVersion"].toString())
+    platform(org.spongepowered.gradle.vanilla.repository.MinecraftPlatform.SERVER)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -31,24 +31,9 @@ tasks {
         logger.warn("All data is given independently without warranty, guarantee or liability of any kind.")
         logger.warn("The data may or may not be the intellectual property of Mojang Studios.")
         logger.warn("")
-        // Here is an example:
-        // We want to run the data generator for the version 1.16.3
-        // This will mean we want to run the code from the 1.16.5 generators with the 1.16.3 JAR on runtime
-        // First of all we will deobf the 1.16.3 JAR and then, to COMPILE the 1.16.5 generators, deobf the 1.16.5 JAR
-        // Then the 1.16.5 generators are included in the runtime of DataGenerator and using reflection they are accessed.
-        // Why use reflection???
-        // --> We want to allow the DataGenerator module to hit any version without having to necessarily compile every version
-        // E.g. If we referenced the 1.17 generator in DataGenerator we NEED it for compile
-        // However if we use reflection we can just "ignore its unavailability in the classpath"
-        // This also allows the 1.17 generator to reference the 1.16.5 generator without much hassle.
-        // As long as the 1.16.5 JAR is also decompiled for compiling the 1.16.5 generators.
 
-        // TL;DR: We decompile one (or more) version for compile, and only ever one for runtime.
-        // Run the deobfuscator
-        dependsOn(project(":Deobfuscator").tasks.getByName<JavaExec>("run") {
-            args = arrayListOf(mcVersion)
-        }).finalizedBy(
-            // Run the DataGenerator
+        // Simplified by Sponge's VanillaGradle
+        dependsOn(
             project(":DataGenerator").tasks.getByName<JavaExec>("run") {
                 args = arrayListOf(outputDirectory)
             }
