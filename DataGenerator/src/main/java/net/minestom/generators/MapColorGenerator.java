edf64b7a6df79608afed79eb8066b4ed09728276
TheMode <themode@outlook.fr>
2021-12-31 14:37:48 +0100

    1.18 (#3)

--- DataGenerator/src/main/java/net/minestom/generators/MapColorGenerator.java
+++ DataGenerator/src/main/java/net/minestom/generators/MapColorGenerator.java
@@ -2,6 +2,7 @@ package net.minestom.generators;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import java.lang.reflect.Field;
 import net.minecraft.world.level.material.MaterialColor;
 import net.minestom.datagen.DataGenerator;
 
@@ -9,7 +10,16 @@ public final class MapColorGenerator extends DataGenerator {
     @Override
     public JsonArray generate() {
         JsonArray mapColors = new JsonArray();
-        for (MaterialColor mc : MaterialColor.MATERIAL_COLORS) {
+        MaterialColor[] colors;
+        try {
+            Field f = MaterialColor.class.getDeclaredField("MATERIAL_COLORS");
+            f.setAccessible(true);
+            colors = (MaterialColor[]) f.get(null);
+        } catch (NoSuchFieldException | IllegalAccessException e ) {
+            return mapColors;
+        }
+
+        for (MaterialColor mc : colors) {
             if (mc == null) {
                 continue;
             }
