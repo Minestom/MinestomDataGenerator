ce826a548455d6a1d598609fe4f9223cceeee34b
Moulberry <james.jenour@protonmail.com>
2021-12-31 21:53:53 +0800

    Add isFullCube

--- DataGenerator/src/main/java/net/minestom/generators/BlockGenerator.java
+++ DataGenerator/src/main/java/net/minestom/generators/BlockGenerator.java
@@ -118,6 +118,9 @@ public final class BlockGenerator extends DataGenerator {
         appendState(blockJson, state, "interactionShape", blockState.getInteractionShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO).toAabbs().toString(), String.class);
         appendState(blockJson, state, "occlusionShape", blockState.getOcclusionShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO).toAabbs().toString(), String.class);
         appendState(blockJson, state, "visualShape", blockState.getVisualShape(EmptyBlockGetter.INSTANCE, BlockPos.ZERO, CollisionContext.empty()).toAabbs().toString(), String.class);
+        try {
+            appendState(blockJson, state, "isFullCube", blockState.isCollisionShapeFullBlock(null, null), false, boolean.class);
+        } catch(Exception ignored) {}
     }
 
     private <T> void appendState(JsonObject main, JsonObject state, String key, T value, T defaultValue, Class<T> valueType) {
