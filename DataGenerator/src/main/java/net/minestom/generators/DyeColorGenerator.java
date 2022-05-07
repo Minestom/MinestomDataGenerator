c2a3e0cf164c5d30f53550e35204545e7b2d085a
Articdive <13535885+Articdive@users.noreply.github.com>
2022-05-07 11:10:53 +0200

    Add Dye Colors. (#9)

---
+++ DataGenerator/src/main/java/net/minestom/generators/DyeColorGenerator.java
@@ -0,0 +1,24 @@
+package net.minestom.generators;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import net.minecraft.world.item.DyeColor;
+import net.minestom.datagen.DataGenerator;
+
+public final class DyeColorGenerator extends DataGenerator {
+    @Override
+    public JsonArray generate() {
+        JsonArray dyeColors = new JsonArray();
+
+        for (DyeColor dC : DyeColor.values()) {
+            JsonObject dyeColor = new JsonObject();
+            dyeColor.addProperty("id", dC.getId());
+            dyeColor.addProperty("name", dC.name());
+            dyeColor.addProperty("textColor", dC.getTextColor());
+            dyeColor.addProperty("fireworkColor", dC.getFireworkColor());
+            dyeColor.addProperty("mapColor", dC.getMaterialColor().id);
+            dyeColors.add(dyeColor);
+        }
+        return dyeColors;
+    }
+}
