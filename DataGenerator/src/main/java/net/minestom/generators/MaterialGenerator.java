5fbd8dd536660a02e8e9d5497d1db8934acba186
kermandev <46640204+kermandev@users.noreply.github.com>
2026-01-13 14:09:49 -0600

    refactor: Update to snapshot3

--- DataGenerator/src/main/java/net/minestom/generators/MaterialGenerator.java
+++ DataGenerator/src/main/java/net/minestom/generators/MaterialGenerator.java
@@ -3,10 +3,15 @@ package net.minestom.generators;
 import com.google.gson.JsonObject;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.JsonOps;
+import net.minecraft.core.DefaultedRegistry;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistryAccess;
+import net.minecraft.core.component.DataComponentInitializers;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.data.registries.VanillaRegistries;
 import net.minecraft.resources.RegistryOps;
 import net.minecraft.util.Util;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minestom.datagen.DataGenerator;
@@ -20,8 +25,8 @@ public final class MaterialGenerator extends DataGenerator {
         var blockRegistry = BuiltInRegistries.BLOCK;
         var entityTypeRegistry = BuiltInRegistries.ENTITY_TYPE;
 
-        var registryAccess = RegistryAccess.fromRegistryOfRegistries(BuiltInRegistries.REGISTRY);
-        var registryJsonOps = RegistryOps.create(JsonOps.INSTANCE, registryAccess);
+        var lookup = applyPendingComponents();
+        var registryJsonOps = lookup.createSerializationContext(JsonOps.INSTANCE);
 
         for (var item : registry) {
             final var location = registry.getKey(item);
@@ -57,5 +62,12 @@ public final class MaterialGenerator extends DataGenerator {
         } else {
             throw new RuntimeException("Unknown error");
         }
+    }
+
+    private static HolderLookup.Provider applyPendingComponents() {
+        HolderLookup.Provider lookup = VanillaRegistries.createLookup();
+        var registry = BuiltInRegistries.DATA_COMPONENT_INITIALIZERS.build(lookup);
+        registry.forEach(DataComponentInitializers.PendingComponents::apply);
+        return lookup;
     }
 }
